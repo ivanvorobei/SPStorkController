0f5628a7422daed1252bfa89363b0bd92d57984c
Sjoerd Janssen <sjoerd@blauwprint.io>
2019-05-01 08:38:39 +0200

    implement a haptic enum

---
+++ Source/SPStorkController/Enums/SPStorkHapticMoments.swift
@@ -0,0 +1,17 @@
+//
+//  SPStorkHapticMoments.swift
+//  stork-controller
+//
+//  Created by Sjoerd Janssen on 10/04/2019.
+//  Copyright © 2019 Ivan Vorobei. All rights reserved.
+//
+
+import UIKit
+
+public enum SPStorkHapticMoments {
+	case whilstPresenting
+	case afterPresenting
+	case whilstDismissing
+	case afterDismissing
+	case none
+}
