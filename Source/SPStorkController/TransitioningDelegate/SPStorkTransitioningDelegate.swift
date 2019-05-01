f33ce79ee4bc771ad3e54f26b84f7521d0658678
Sjoerd Janssen <sjoerd@blauwprint.io>
2019-05-01 08:38:47 +0200

    add support for haptic moments

--- Source/SPStorkController/TransitioningDelegate/SPStorkTransitioningDelegate.swift
+++ Source/SPStorkController/TransitioningDelegate/SPStorkTransitioningDelegate.swift
@@ -32,6 +32,7 @@ public final class SPStorkTransitioningDelegate: NSObject, UIViewControllerTrans
     public var customHeight: CGFloat? = nil
     public var translateForDismiss: CGFloat = 200
     public var cornerRadius: CGFloat = 10
+    public var hapticMoments: [SPStorkHapticMoments] = [.whilstPresenting, .whilstDismissing]
     public weak var storkDelegate: SPStorkControllerDelegate? = nil
     
     public func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
@@ -46,6 +47,7 @@ public final class SPStorkTransitioningDelegate: NSObject, UIViewControllerTrans
         controller.translateForDismiss = self.translateForDismiss
         controller.cornerRadius = self.cornerRadius
         controller.transitioningDelegate = self
+        controller.hapticMoments = self.hapticMoments
         controller.storkDelegate = self.storkDelegate
         return controller
     }
