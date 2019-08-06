578b3dbb41c4a6818f5bce608fb025aa6595cb24
Ivan Vorobei <hello@ivanvorobei.io>
2019-08-06 11:03:56 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,21 +1,14 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "SPStorkController",
-    platforms: [.iOS(.v10)],
+    platforms: [
+       .iOS(.v10)
+    ],
     products: [
-        .library(
-            name: "SPStorkController",
-            targets: ["SPStorkController"]),
+        .library(name: "SPStorkController", targets: ["SPStorkController"])
     ],
     targets: [
-        .target(
-            name: "SPStorkController",
-            dependencies: [],
-            path: "Source/SPStorkController"
-            )
+        .target(name: "SPStorkController", dependencies: [], path: "Source/SPStorkController")
     ]
 )
